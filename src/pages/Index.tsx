e148d3973ca7ffe65c51b6459dcce8a22c65decf
TioPhillBR <phelipeac3@gmail.com>
2026-03-19 00:05:30 -0300

    migracao
    migracao

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-background">
-      <p className="text-sm text-muted-foreground">Projeto em branco pronto.</p>
-    </div>
-  );
-};
-
-export default Index;
